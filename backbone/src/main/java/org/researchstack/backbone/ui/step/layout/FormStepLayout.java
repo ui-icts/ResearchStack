c839aa8919e54b0b01d25955d9173bc2d446fabd
Sam Martinez <samanatha-martinez@uiowa.com>
2017-03-03 15:41:22 -0600

    Fix next button behavior in FormStep

--- backbone/src/main/java/org/researchstack/backbone/ui/step/layout/FormStepLayout.java
+++ backbone/src/main/java/org/researchstack/backbone/ui/step/layout/FormStepLayout.java
@@ -2,9 +2,17 @@ package org.researchstack.backbone.ui.step.layout;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
+import org.researchstack.backbone.ui.callbacks.StepCallbacks;
+import org.researchstack.backbone.ui.step.body.BodyAnswer;
+
 
 /**
- * Created by Samantha on 3/2/2017.
+ * Layout for FormStep which ensures proper behavior of the 'next' button; causes 'next'
+ * to shift focus to the next text field or to submit form if no fields are left.
+ *
+ * Created by sdmartinez on 3/2/2017.
  */
 public class FormStepLayout extends SurveyStepLayout {
 
@@ -19,6 +27,32 @@ public class FormStepLayout extends SurveyStepLayout {
 
     public FormStepLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onNextClicked() {
+
+        BodyAnswer bodyAnswer = stepBody.getBodyAnswerState();
+        View currentView = container.findFocus();
+        View nextView = container.focusSearch(currentView, FOCUS_DOWN);
+
+        if (nextView != null) {
+            nextView.requestFocus();
+        }
+        else if (bodyAnswer == null || !bodyAnswer.isValid()) {
+            Toast.makeText(
+                    getContext(),
+                    bodyAnswer == null
+                            ? BodyAnswer.INVALID.getString(getContext())
+                            : bodyAnswer.getString(getContext()),
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            callbacks.onSaveStep(
+                    StepCallbacks.ACTION_NEXT,
+                    getStep(),
+                    stepBody.getStepResult(false));
+        }
     }
 
 }
